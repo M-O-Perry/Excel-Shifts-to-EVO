6658622afc29dd3da662a49cbe659f79b4140c7b
BobBobilion <ahmadirad174@gmail.com>
2024-08-26 17:47:09 -0700

    rerouted the whole shift adding

---
+++ TASFiles.py
@@ -0,0 +1,8 @@
+class TASFileReferences:
+    def __init__(self):
+        
+        self.references = {
+            "MDB" : ("\\\\ERP\\dbamfg\\T7WOM.RWN", 30),
+            "WOF" : ("\\\\ERP\\dbamfg\\T7WOF.RWN", 2),
+            
+        }
--- UploadShifts.py
+++ UploadShifts.py
@@ -1,17 +1,25 @@
+import datetime
 from PlayActions import send_keys as send
 
 from tkinter import filedialog, simpledialog, messagebox
 import os
 import openpyxl
+from EVOUtil import openTASProgram
 
 def getInputs():
+    # get employee ID
+    employeeID = simpledialog.askinteger("Employee ID", "Enter the employee ID")
+    
+    if employeeID is None:
+        os._exit(0)
+    
     file = filedialog.askopenfilename()
     if file == "":
         os._exit(0)
     
-    return file
+    return employeeID, file
 
-file = getInputs()
+employeeID, file = getInputs()
 
 def getTimes(excelFile):
     wb = openpyxl.load_workbook(excelFile, data_only=True)
@@ -32,22 +40,22 @@ def getTimes(excelFile):
     return times
 
 
-def openShifts():
-    send(["focus EVO ~ ERP", 0.5])
-    send(["alt m m d m", 12])
-    send(["tab", "right 2", 0.5, "enter", 0.5])
+# def openShifts():
+#     openTASProgram("WOF")
+#     send(["focus WO-M", 5])
+#     send(["tab", "right 2", 0.5, "enter", 0.5])
 
-def addShift(employeeID, operation, sequence, hours, date = ""):
-    if date == "":
-        send(["enter"])
-    else:
-        send(["#"+str(date), "enter"])
+# def addShift(employeeID, operation, sequence, hours, date = ""):
+#     if date == "":
+#         send(["enter"])
+#     else:
+#         send(["#"+str(date), "enter"])
     
-    send(["#"+str(employeeID), "enter", "#"+str(operation), "enter", "#"+str(sequence), "enter 4", "#"+str(hours), "alt s",2])
-    print("added")
+#     send(["#"+str(employeeID), "enter", "#"+str(operation), "enter", "#"+str(sequence), "enter 4", "#"+str(hours), "alt s",2])
+#     print("added")
 
-def closeShifts():
-    send(["alt x", 0.5])
+# def closeShifts():
+#     send(["alt x", 0.5])
 
 def is_float(element: any) -> bool:
     #If you expect None to be passed:
@@ -60,12 +68,26 @@ def is_float(element: any) -> bool:
         return False
     
 allShifts = getTimes(file)
-openShifts()
-print(allShifts)
-for shift in allShifts:
-    print(shift)
-    addShift(214, shift[0], shift[1], shift[2], "010119")
 
-closeShifts()
+# openShifts()
+# print(allShifts)
+# for shift in allShifts:
+#     print(shift)
+#     addShift(250, shift[0], shift[1], shift[2], datetime.datetime.today().strftime('%m/%d/%Y'))
+
+# closeShifts()
+
+
+def addshift(employeeID, operation, sequence, hours):
+    send(["enter", 0.5])
+    send(["#"+str(employeeID),  "enter",0.5, "#"+str(operation),  "enter",0.5, "#"+str(sequence), "enter 8", "#"+str(hours), "enter", "alt s", 1, "enter", 1, "alt x", 1])
+
+def addAllShifts(shifts):
+    
+    for shift in shifts:
+        openTASProgram("WOF")
+        addshift(employeeID, shift[0], shift[1], shift[2])
+
+addAllShifts(allShifts)
 
 print("Shifts Added", "Shifts have been added to the system.")
